2e65550dc95a2897ec545af31550c56794622b0e
Ian Henisa <37345944+ihenisa1972@users.noreply.github.com>
2019-06-26 13:34:00 -0400

    Add ReadLine() code

--- FileIO.py
+++ FileIO.py
@@ -1,4 +1,5 @@
-input_file = open("hello.txt", "w")
+# Readlines() example
+"""input_file = open("hello.txt", "w")
 
 lines_to_write = [
     "This is line 1\n",
@@ -14,7 +15,22 @@ read_data = output_file.readlines()
 for line in read_data:
     print(line)
 
-output_file.close()
+output_file.close()"""
+
+# Readline() example
+input_file = open("hello.txt", "r")
+
+file_input = input_file.readline()
+
+while file_input != "":
+    print(file_input)
+    file_input = input_file.readline()
+
+input_file.close()
+
+
+
+
 
 
 
